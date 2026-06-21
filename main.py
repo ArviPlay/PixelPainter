3993c1883434e6511b61f49c86f606f076a560d7
Arvi <artemtolstykn@gmail.com>
2026-06-21 12:16:14 +0300

    extract settings to config.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import pyautogui
 import keyboard, mouse
 import time, os
+import config
 
 path = input("Image path: ")
 image = Image.open(path.strip('"'))
@@ -9,9 +10,9 @@ gray_img = image.convert("L")
 
 is_invert = input("invert image? (1 - yes, default - no): ")
 if is_invert == "1":
-    bw_img = gray_img.point(lambda x: 255 if x < 128 else 0, '1')
+    bw_img = gray_img.point(lambda x: 255 if x < config.THRESHOLD else 0, '1')
 else:
-    bw_img = gray_img.point(lambda x: 0 if x < 128 else 255, '1')
+    bw_img = gray_img.point(lambda x: 0 if x < config.THRESHOLD else 255, '1')
 
 
 upper_left = list()
@@ -38,7 +39,7 @@ while(True): #setting the lower right corner coordinate
 
 final_img = bw_img.resize((lower_right[0]-upper_left[0], lower_right[1]-upper_left[1])) #resizing image
 
-between_clicks_delay = 0.003
+between_clicks_delay = config.DEFAULT_DELAY
 between_clicks_delay_input = input("between clicks delay in seconds (default: 0.003): ")
 if between_clicks_delay_input: between_clicks_delay = float(between_clicks_delay_input) #setting delay
 
@@ -46,6 +47,7 @@ os.system('cls')
 print(f"start X: {upper_left[0]}; start Y: {upper_left[1]}")
 print(f"size X: {lower_right[0]-upper_left[0]}; size Y: {lower_right[1]-upper_left[1]}") #printing information
 print(f"delay: {between_clicks_delay}")
+print(f"threshold: {config.THRESHOLD}")
 print("escape - break")
 print("press enter to start...")
 keyboard.wait('enter')
